907381c6255e37aaa7bba3b440f468a928840d3b
Enrique Balleste <eballeste@divisionof.com>
2018-12-16 19:24:58 -0500

    allows importing relative to src directory
    - simpler than adding aliases through gatsby plugin-root-import

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,12 +16,6 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-sass',
-    {
-      resolve: 'gatsby-plugin-root-import',
-      options: {
-        components: `${__dirname}/src/components`,
-        styles: `${__dirname}/src/styles`,
-      },
-    },
+    'gatsby-plugin-resolve-src',
   ],
 }
