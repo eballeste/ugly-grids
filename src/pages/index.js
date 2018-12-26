0d894267f95b976f0c9d351d61d4964eb07a8915
Enrique Balleste <eballeste@divisionof.com>
2018-12-26 14:42:24 -0500

    :wrench: keeps things more semantical

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,14 @@ import styles from 'styles/pages/index.module.scss'
 
 const IndexPage = () => (
   <Layout>
-    <main className={styles.main}>
-      <div className={styles.main__titlebox}>
-        <div className={styles.main__title}><h1>CSS Grid Experiments</h1></div>
-      </div>
-      <div className={styles.main__listbox}>
-        <div className={styles.main__link} onClick={() => navigate('/exp-1')}>1</div>
-      </div>
-    </main>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.header__title}><h1>CSS Grid Experiments</h1></div>
+      </header>
+      <nav className={styles.links}>
+        <div className={styles.links__link} onClick={() => navigate('/exp-1')}>1</div>
+      </nav>
+    </div>
   </Layout>
 )
 
