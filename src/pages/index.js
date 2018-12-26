076405668b90f100a3d6fdfc820ecbccc48931cd
Enrique Balleste <eballeste@divisionof.com>
2018-12-26 13:06:55 -0500

    updates landing page
    - improved grid styles
    - link setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { navigate } from 'gatsby'
 
 import Layout from 'components/layout'
 
@@ -7,7 +8,12 @@ import styles from 'styles/pages/index.module.scss'
 const IndexPage = () => (
   <Layout>
     <main className={styles.main}>
-      <div className={styles.title}><h1>CSS Grid Experiments</h1></div>
+      <div className={styles.main__titlebox}>
+        <div className={styles.main__title}><h1>CSS Grid Experiments</h1></div>
+      </div>
+      <div className={styles.main__listbox}>
+        <div className={styles.main__link} onClick={() => navigate('/exp-1')}>1</div>
+      </div>
     </main>
   </Layout>
 )
